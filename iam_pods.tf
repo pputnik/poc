492e88c4587d6c20e41485a740a6ba647bc29f23
alutchko <alexander.lutchko@360codelab.com>
2020-07-30 13:16:25 +0200

    roles changes

--- iam_pods.tf
+++ iam_pods.tf
@@ -1,32 +1,3 @@
-### node role
-data "template_file" "node_role_policy" {
-  template = file("oidc_assume_role_policy.json")
-  vars = {
-    OIDC_ARN  = aws_iam_openid_connect_provider.cluster.arn
-    OIDC_URL  = replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")
-    NAMESPACE = "kube-system"
-    SA_NAME   = "aws-node"
-  }
-}
-
-resource "aws_iam_role" "pod2" {
-  name = "${var.cluster_name}-aws-node"
-  //assume_role_policy =  templatefile("oidc_assume_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.cluster.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.cluster.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
-  assume_role_policy =  data.template_file.node_role_policy.rendered
-
-  tags = merge(var.tags, {
-      "ServiceAccountName"      = "aws-node"
-      "ServiceAccountNameSpace" = "kube-system"
-  })
-  depends_on = [aws_iam_openid_connect_provider.cluster]
-}
-
-resource "aws_iam_role_policy_attachment" "aws_node" {
-  role       = aws_iam_role.pod2.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  depends_on = [aws_iam_role.pod2]
-}
-
 ### role to run POD1
 /*resource "aws_iam_role" "pod1" {
 	name_prefix        = "${var.tags["project"]}_pod1"
--- nodegroup.tf
+++ nodegroup.tf
@@ -24,12 +24,11 @@ resource "aws_eks_node_group" "example" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.aws_node,
     aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks_AmazonEKSServicePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly
   ]
 
   tags = merge(var.tags, {
@@ -37,8 +36,36 @@ resource "aws_eks_node_group" "example" {
   })
 }
 
+### node role
+data "template_file" "node_role_policy" {
+  template = file("oidc_assume_role_policy.json")
+  vars = {
+    OIDC_ARN  = aws_iam_openid_connect_provider.cluster.arn
+    OIDC_URL  = replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")
+    NAMESPACE = "kube-system"
+    SA_NAME   = "aws-node"
+  }
+}
+
 resource "aws_iam_role" "eks_node" {
   name = "eks-node-group-example"
+  //assume_role_policy =  templatefile("oidc_assume_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.cluster.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.cluster.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
+  assume_role_policy =  data.template_file.node_role_policy.rendered
+
+  tags = merge(var.tags, {
+      "ServiceAccountName"      = "aws-node"
+      "ServiceAccountNameSpace" = "kube-system"
+  })
+  depends_on = [aws_iam_openid_connect_provider.cluster]
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+/*resource "aws_iam_role" "eks_node_bad" {
+  name = "eks-node-group-example-bad"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -50,19 +77,14 @@ resource "aws_iam_role" "eks_node" {
     }]
     Version = "2012-10-17"
   })
-}
+}*/
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_node.name
 }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node.name
 }
