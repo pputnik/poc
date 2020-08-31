5f9882896f712ed3c7f89bf02b39f53e052b632a
alutchko <alexander.lutchko@360codelab.com>
2020-08-31 13:48:41 +0200

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  environment = var.workspace_to_environment[terraform.workspace]
+  environment = var.workspace_to_environment["test"]
   acc_id      = data.aws_caller_identity.current.account_id
 }
 
