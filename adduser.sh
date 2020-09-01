9718ef57d2579070fa6bc4a8edb6f07c46f8ceab
alutchko <alexander.lutchko@360codelab.com>
2020-09-01 20:39:32 +0200

    script test3

--- adduser.sh
+++ adduser.sh
@@ -5,5 +5,5 @@ q2="GRANT all  ON iamtest.* TO 'lambda-user'@'%';"
 r1=$(echo $q1 | mysql -v -h$1 -u$2 -p$3 2>&1)
 r2=$(echo $q2 | mysql -v -h$1 -u$2 -p$3 2>&1)
 
-echo '{"r1":"$r1", "r2":"$r2"}'
+echo "{\"r1\":\"$r1\", \"r2\":\"$r2\"}"
 exit 0
