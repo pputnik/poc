878d96b3807735ccb27d5758fe92d4c72a6b4010
alutchko <alexander.lutchko@360codelab.com>
2020-09-01 14:24:19 +0200

    create3

--- adduser.sh
+++ adduser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo '{"q":"a"}'
+exit 0
 echo "CREATE USER 'lambda-user'@'%' IDENTIFIED WITH AWSAuthenticationPlugin as 'RDS';"
 
 echo "GRANT all  ON iamtest.* TO 'lambda-user'@'%';"
