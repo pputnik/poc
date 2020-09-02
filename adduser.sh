655a2f47b9363401dbd33fee156cbe64a449276f
alutchko <alexander.lutchko@360codelab.com>
2020-09-02 08:49:08 +0200

    create

--- adduser.sh
+++ adduser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-q1="CREATE USER 'lambda-user'@'%' IDENTIFIED WITH AWSAuthenticationPlugin as 'RDS'"
+q1="CREATE USER IF NOT EXISTS 'lambda-user'@'%' IDENTIFIED WITH AWSAuthenticationPlugin as 'RDS'"
 q2="GRANT all  ON iamtest.* TO 'lambda-user'@'%';"
 
 r1=$(echo $q1 | mysql -v -h$1 -u$2 -p$3 2>&1)
