2c2d83f7cce90605fc693fc81f28ec83b54e716e
alutchko <alexander.lutchko@360codelab.com>
2020-09-03 15:46:27 +0200

    min test2

--- main.tf
+++ main.tf
@@ -1,24 +1,9 @@
-data "aws_caller_identity" "current" {}
-
 locals {
   environment = var.workspace_to_environment["test"]
-  acc_id      = data.aws_caller_identity.current.account_id
 }
 
 variable "project" {
   default = "bug-test"
-}
-
-data "terraform_remote_state" "infrastructure" {
-  backend = "s3"
-
-  config = {
-    #bucket   = "${var.vpc_remote_state_bucket_base}.${var.environment}"
-    bucket   = "com.dodax.infrastructure.terraform.testing"
-    key      = "infrastructure-vpc/terraform.tfstate"
-    region   = "eu-central-1"
-    role_arn = "arn:aws:iam::609350192073:role/jenkins_executor"
-  }
 }
 
 resource "aws_vpc" "vpc" {
@@ -32,8 +17,8 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_vpc_peering_connection" "peer-infrastructure" {
   vpc_id        = aws_vpc.vpc.id
-  peer_vpc_id   = data.terraform_remote_state.infrastructure.outputs.vpc-id
-  peer_owner_id = data.terraform_remote_state.infrastructure.outputs.account-id
+  peer_vpc_id   = "vpc-0973f761"
+  peer_owner_id = "313829517975"
   peer_region   = "eu-central-1"
   auto_accept   = true
 
