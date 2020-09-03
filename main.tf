041ad35f3c6afc3e39a3b765b58280e4cb2d845e
alutchko <alexander.lutchko@360codelab.com>
2020-09-03 12:22:37 +0200

    args

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ locals {
 
 module "vpc" {
   source       = "git@github.com:dodax/terraform-aws-vpc?ref=workspaces"
-  environment  = terraform.workspace
-  project_name = var.tags["project"]
+  project = var.tags["project"]
 
   vpc_cidr = "${var.cidr_head}.128.0/17"
   subnet_cidr_public = {
