41478cbc99174e40ea5a8d7315afcfda040e26d7
Alex <alex@classerium.biz>
2023-01-22 18:06:11 +0100

    vars

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.50"
     }
   }
 }
@@ -15,7 +15,7 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr
 
 }
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+
 variable "region" {
   default = "eu-central-1"
 }
