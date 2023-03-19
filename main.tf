23a5cc64d78718f864eea775f6546f8a2814ae40
pputnik <pputnik@gmail.com>
2023-03-19 18:29:39 +0100

    egress

--- main.tf
+++ main.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "web" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
 
 data "template_file" "user_data" {
