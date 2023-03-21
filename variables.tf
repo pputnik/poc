c9e4a2bb78b613d6ccc1ff040b1c2b5e77b9b19d
pputnik <pputnik@gmail.com>
2023-03-21 15:13:44 +0100

    sec manag

--- variables.tf
+++ variables.tf
@@ -34,6 +34,16 @@ variable "ports" {
 #  description = "input number here"
 #}
 
+variable "ami" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+
+  validation {
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "The 'ami' value must be a valid AMI id, starting with '\"'ami-'\"'."
+  }
+}
+
 variable "region" {
   default = "eu-west-1"
 }
