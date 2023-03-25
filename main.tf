17b979fde15496946e89d659e62972e89483f6f8
pputnik <pputnik@gmail.com>
2023-03-25 20:20:28 +0100

    if

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ output "def_out" {
 }
 
 output "is_prod" {
-  value = values(aws_secretsmanager_secret.supersecret)["x22"].id
+  value = values(aws_secretsmanager_secret.supersecret)["x22"]
 }
 
 
