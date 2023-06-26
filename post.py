9999a8feda7a874da4832c9d503c9af4a6e9344e
pputnik <pputnik@gmail.com>
2023-06-26 16:05:21 +0200

    -aaa

--- post.py
+++ post.py
@@ -1,13 +1,13 @@
 import requests
 
-# import yaml
-# import datetime
+import yaml
+import datetime
 
-# now_d = datetime.datetime.now()
+now_d = datetime.datetime.now()
 
-now_s = "q"  # now_d.strftime("%Y-%m-%d %H:%M:%S")
+now_s = now_d.strftime("%Y-%m-%d %H:%M:%S")
 
-print(f"translation={now_s}")
+print(f"now_s={now_s}")
 
 # load config
 with open("../tlg.yml", 'r') as stream:
@@ -17,5 +17,5 @@ tlg_token = data_loaded['tlg_token']
 chat_id = data_loaded['chat_id']
 
 # send msg
-# url = f"https://api.telegram.org/bot{tlg_token}/sendMessage?chat_id={chat_id}&text={now_s}"
-# print(requests.get(url).json())
+url = f"https://api.telegram.org/bot{tlg_token}/sendMessage?chat_id={chat_id}&text={now_s}"
+print(requests.get(url).json())
--- tmp.py
+++
@@ -1,2 +0,0 @@
-aaa = 3
-print(f"aaa={aaa}")
