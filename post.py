2234a9ed75636472e83b0bd65c27ed1e29814cec
pputnik <pputnik@gmail.com>
2023-06-26 15:50:48 +0200

    test msg

--- post.py
+++ post.py
@@ -1,7 +1,17 @@
 import requests
 import yaml
+import datetime
+
+now = datetime.datetime.now()
+
+now_s = strftime("%Y-%m-%d %H:%M:%S", now)
+print(f"now_s={now_s}")
 
 with open("../tlg.yml", 'r') as stream:
   data_loaded = yaml.safe_load(stream)
 
-print(data_loaded)
+tlg_token = data_loaded['tlg_token']
+chat_id = data_loaded['chat_id']
+
+url = f"https://api.telegram.org/bot{tlg_token}/sendMessage?chat_id={chat_id}&text={now_s}"
+print(requests.get(url).json())
