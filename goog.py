b08681dd111ce97383e775b94599e7f06ade5d63
Alex <alex@classerium.biz>
2023-06-26 19:03:09 +0200

    path

--- goog.py
+++ goog.py
@@ -33,7 +33,7 @@ def main():
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-        'creds.json', SCOPES)
+        '../creds.json', SCOPES)
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
     with open('token.json', 'w') as token:
