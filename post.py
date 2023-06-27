e680dae65feb38b262c8352092b7e26d31775ee9
Alex <alex@classerium.biz>
2023-06-27 07:44:24 +0200

    send_photo

--- post.py
+++ post.py
@@ -3,6 +3,7 @@ import requests
 import yaml
 from datetime import datetime
 
+DOMAIN = "https://api.telegram.org"
 tlg_token = ''
 
 def main():
@@ -29,21 +30,21 @@ def main():
   mydata = gsheet.sheet1.get_all_records()
 
   wsheet = gsheet.worksheet("default")
-  rownum = 1  # the first line is header so we need to skip it
+  rownum = 1  # the first line is header, so we need to skip it
 
   for line in mydata:
     rownum = rownum + 1
-    print(f"{rownum} {line}")
-    if not line['Scheduled'] or not line['Text']:  # just strange line, maybe draft
+    # print(f"{rownum} {line}")
+    if not line['Scheduled'] or not line['Text']:
+      print(f"{rownum} ignore: no 'Scheduled' or 'Text', just strange line, maybe draft {line}")
       continue
 
     try:
       scheduled = datetime.strptime(line['Scheduled'], '%b/%d/%Y %H:%M')
     except Exception as e:
-      print("ERR: this line does not contain valid Scheduled date, will be skipped")
+      print(f"{rownum} ERR: this line does not contain valid Scheduled date, will be skipped {line}")
       print(e)
       print("--------------------------")
-      break
       continue
 
     print(f"scheduled={scheduled}")
@@ -55,7 +56,14 @@ def main():
         chat = chat_id
 
       try:
-        result = post(chat_id, line['Text'])
+        print(f"{rownum} sending... {line}")
+        if line['Img URL']:
+          if line['NSFW']:
+            result = send_photo(chat, line['Text'], line['Img URL'], True)
+          else:
+            result = send_photo(chat, line['Text'], line['Img URL'])
+        else:
+          result = send_message(chat, line['Text'])
       except Exception as e:
         print("ERR: can't post this message, something's wrong")
         print(e)
@@ -69,16 +77,29 @@ def main():
       wsheet.update('B' + str(rownum), now_s)
 
 
-def post(chat_id, message):
+def send_photo(chat_id, message, url, nsfw=False):
   global tlg_token
 
   # send msg
-  print("posting")
-  url = f"https://api.telegram.org/bot{tlg_token}/sendMessage?chat_id={chat_id}&text={message}"
+  print("sendPhoto")
+  url = f"{DOMAIN}/bot{tlg_token}/sendPhoto?chat_id={chat_id}&photo={url}&caption={message}&has_spoiler={nsfw}"
   result = requests.get(url).json()
   print(result)
 
   return result['ok']
+
+
+def send_message(chat_id, message):
+  global tlg_token
+
+  # send msg
+  print("sendMessage")
+  url = f"{DOMAIN}/bot{tlg_token}/sendMessage?chat_id={chat_id}&text={message}"
+  result = requests.get(url).json()
+  print(result)
+
+  return result['ok']
+
 
 if __name__ == '__main__':
   main()
